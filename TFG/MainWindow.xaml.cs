76731476371b24702cf03cd7863b339b9b9409ff
Ruben Agudo Santos <rubenagudo21@hotmail.com>
2013-11-19 22:05:26 +0100

    Some testing done. Putting video controls inside a toolbox and buttons for the media playback.

--- TFG/MainWindow.xaml.cs
+++ TFG/MainWindow.xaml.cs
@@ -23,5 +23,44 @@ namespace TFG
         {
             InitializeComponent();
         }
+
+      
+        // Play the media. 
+        void OnMouseDownPlayMedia(object sender, RoutedEventArgs args)
+        {
+
+            // The Play method will begin the media if it is not currently active or  
+            // resume media if it is paused. This has no effect if the media is 
+            // already running.
+            myMediaElement.Play();
+
+        }
+
+        // Pause the media. 
+        void OnMouseDownPauseMedia(object sender, RoutedEventArgs args)
+        {
+
+            // The Pause method pauses the media if it is currently running. 
+            // The Play method can be used to resume.
+            myMediaElement.Pause();
+
+        }
+
+        // Stop the media. 
+        void OnMouseDownStopMedia(object sender, RoutedEventArgs args)
+        {
+
+            // The Stop method stops and resets the media to be played from 
+            // the beginning.
+            myMediaElement.Stop();
+
+        }
+
+        // When the media playback is finished. Stop() the media to seek to media start. 
+        private void Element_MediaEnded(object sender, EventArgs e)
+        {
+            myMediaElement.Stop();
+        }
+
     }
 }
